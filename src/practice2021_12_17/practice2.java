311d846dacfa878ad8d1044538bbc63cb1dd2f44
addwater1 <shokuhou93@gmail.com>
2022-01-02 16:11:26 +0800

    ...

--- src/practice2021_12_17/practice2.java
+++ src/practice2021_12_17/practice2.java
@@ -1,12 +1,17 @@
 package practice2021_12_17;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Collections;
 
 public class practice2 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		FileWriter f = null;
+		BufferedWriter fl = null;
 		Scanner input = new Scanner(System.in);
 		ArrayList<student> students = new ArrayList<>();
 		String s;
@@ -21,9 +26,13 @@ public class practice2 {
 			students.add(new student(temp[0],temp[1],temp[2]));
 		}
 		Collections.sort(students,(s1,s2)->(s1.getAge()-s2.getAge()));
-		for (student var : students) {
-			System.out.println(var);
+		f = new FileWriter("src\\output.txt");
+		fl = new BufferedWriter(f);
+		for(student var : students) {
+			fl.write(var + " ");
+			fl.newLine();
 		}
+		fl.close();
 	}
 
 }
